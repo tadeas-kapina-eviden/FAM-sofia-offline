c5db3ea790a00b977995e6c9ada56a624f0c812a
Kapina <tadeas.kapina@atos.net>
2022-08-22 01:59:03 +0200

    created data model for property

---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/model/PropertyPreviewModel.kt
@@ -0,0 +1,8 @@
+package sk.msvvas.sofia.fam.offline.data.model
+
+data class PropertyPreviewModel(
+    val textMainNumber: String,
+    val propertyNumber: String,
+    val subNumber: String,
+    val status: Char
+)
