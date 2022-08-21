d03e04c14da16ba533b5a1507dddf94e8bc4f5b0
Kapina <tadeas.kapina@atos.net>
2022-08-22 00:57:52 +0200

    created data model for inventory

---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/model/Inventory.kt
@@ -0,0 +1,12 @@
+package sk.msvvas.sofia.fam.offline.data.model
+
+import java.time.LocalDate
+
+data class Inventory(
+    val id : String,
+    val createdAt : LocalDate,
+    val createdBy : String,
+    var note : String,
+    var countProcessed : Int,
+    var countAll : Int
+)
