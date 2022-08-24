e8f3ba4ee0b98ffdd2dd876dc1ab5e3d25892941
Kapina <tadeas.kapina@atos.net>
2022-08-24 14:55:10 +0200

    created ViewModel for InventoryListView, InventoryListView now show data from local database

--- app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListView.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListView.kt
@@ -1,22 +1,25 @@
-package sk.msvvas.sofia.fam.offline.ui.views.inventory
+package sk.msvvas.sofia.fam.offline.ui.views.inventory.list
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import sk.msvvas.sofia.fam.offline.data.model.InventoryModel
-import sk.msvvas.sofia.fam.offline.ui.views.inventory.list.InventoryListViewModel
 import java.time.LocalDate
 
 @Composable
 fun InventoryListView(
     inventoryListViewModel: InventoryListViewModel
 ) {
+
+    val inventories by inventoryListViewModel.inventories.observeAsState(emptyList())
 
     Column(
         modifier = Modifier
@@ -29,7 +32,8 @@ fun InventoryListView(
                 .padding(15.dp)
                 .fillMaxWidth()
         )
-        inventoryListViewModel.inventories.value!!.forEach {
+
+        inventories.forEach {
             InventoryListItem(inventory = it)
         }
     }
