26496ad94b5001c8d55fc5f9f0913e1037933883
Kapina <tadeas.kapina@atos.net>
2022-08-25 00:23:38 +0200

    added inventory items download verification

--- app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListView.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListView.kt
@@ -1,13 +1,15 @@
 package sk.msvvas.sofia.fam.offline.ui.views.inventory.list
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,26 +22,75 @@ fun InventoryListView(
 ) {
 
     val inventories by inventoryListViewModel.inventories.observeAsState(emptyList())
+    val selectedInventoryId by inventoryListViewModel.selectedInventoryId.observeAsState("")
+    val isDownloadConfirmShown by inventoryListViewModel.isDownloadConfirmShown.observeAsState(false)
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Text(
-            text = "Vyber Inventúru",
-            textAlign = TextAlign.Center,
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
             modifier = Modifier
-                .padding(15.dp)
                 .fillMaxWidth()
-        )
-
-        inventories.forEach {
-            InventoryListItem(
-                inventory = it,
-                onClick = {
-                    inventoryListViewModel.onSelectInventory(it.id)
-                }
+        ) {
+            Text(
+                text = "Vyber Inventúru",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxWidth()
             )
+
+            inventories.forEach {
+                InventoryListItem(
+                    inventory = it,
+                    onClick = {
+                        inventoryListViewModel.onSelectInventory(it.id)
+                    }
+                )
+            }
+        }
+        if (isDownloadConfirmShown) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize(),
+                color = Color(0xBB222222),
+                content = {}
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .align(Alignment.Center)
+                    .background(color = MaterialTheme.colors.surface)
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colors.primary
+                    ),
+            ) {
+                TextField(
+                    value = "Naozaj chceš stiahnuť Inventúru s id: $selectedInventoryId?",
+                    readOnly = true,
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = MaterialTheme.colors.surface
+                    ),
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally),
+                )
+                Row() {
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        onClick = { inventoryListViewModel.onSelectInventoryConfirm() }
+                    ) {
+                        Text(text = "Áno")
+                    }
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        onClick = { inventoryListViewModel.onSelectInventoryDecline() }
+                    ) {
+                        Text(text = "Nie")
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListViewModel.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListViewModel.kt
@@ -1,6 +1,7 @@
 package sk.msvvas.sofia.fam.offline.ui.views.inventory.list
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import sk.msvvas.sofia.fam.offline.data.entities.InventoryEntity
 import sk.msvvas.sofia.fam.offline.data.repository.InventoryRepository
@@ -12,7 +13,22 @@ class InventoryListViewModel(
     private val _inventories: LiveData<List<InventoryEntity>> = inventoryRepository.allData
     val inventories: LiveData<List<InventoryEntity>> = _inventories
 
+    private val _selectedInventoryId = MutableLiveData("")
+    val selectedInventoryId: LiveData<String> = _selectedInventoryId
+
+    private val _isDownloadConfirmShown = MutableLiveData(false)
+    val isDownloadConfirmShown: LiveData<Boolean> = _isDownloadConfirmShown
+
     fun onSelectInventory(inventoryId: String) {
-        viewChange(inventoryId)
+        _selectedInventoryId.value = inventoryId
+        _isDownloadConfirmShown.value = true
+    }
+
+    fun onSelectInventoryConfirm(){
+        viewChange(_selectedInventoryId.value!!)
+    }
+
+    fun onSelectInventoryDecline(){
+        _isDownloadConfirmShown.value = false
     }
 }
