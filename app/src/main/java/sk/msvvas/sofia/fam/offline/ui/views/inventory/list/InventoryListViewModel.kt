1d48dbc0c1628b07c102d84301726ba5a4469194
Kapina <tadeas.kapina@atos.net>
2022-08-24 14:57:23 +0200

    InventoryListViewModel now take only repository as argument

--- app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListViewModel.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/inventory/list/InventoryListViewModel.kt
@@ -1,18 +1,13 @@
 package sk.msvvas.sofia.fam.offline.ui.views.inventory.list
 
-import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import sk.msvvas.sofia.fam.offline.data.database.FamOfflineDatabase
 import sk.msvvas.sofia.fam.offline.data.entities.InventoryEntity
 import sk.msvvas.sofia.fam.offline.data.repository.InventoryRepository
 
 class InventoryListViewModel(
-    context: Context
+    inventoryRepository: InventoryRepository
 ) : ViewModel() {
-    val database = FamOfflineDatabase.getInstance(context = context)
-    private val inventoryRepository = InventoryRepository(database.inventoryDao())
-
     private val _inventories: LiveData<List<InventoryEntity>> = inventoryRepository.allData
     val inventories: LiveData<List<InventoryEntity>> = _inventories
 }
