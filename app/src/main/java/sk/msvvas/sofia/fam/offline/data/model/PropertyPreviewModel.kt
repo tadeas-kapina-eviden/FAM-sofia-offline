f66ebd1925b9a32ff689e052eaed10b6052b35f7
Kapina <tadeas.kapina@atos.net>
2022-08-26 18:51:20 +0200

    add id parameter to property preview model for functionality

--- app/src/main/java/sk/msvvas/sofia/fam/offline/data/model/PropertyPreviewModel.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/model/PropertyPreviewModel.kt
@@ -1,6 +1,7 @@
 package sk.msvvas.sofia.fam.offline.data.model
 
 data class PropertyPreviewModel(
+    val id: Long,
     val textMainNumber: String,
     val propertyNumber: String,
     val subNumber: String,
