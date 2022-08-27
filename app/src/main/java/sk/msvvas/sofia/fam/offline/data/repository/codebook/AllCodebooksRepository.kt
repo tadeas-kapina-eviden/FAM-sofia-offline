186beca584f8eca562da3b441cedae53131ecbbd
Kapina <tadeas.kapina@atos.net>
2022-08-27 16:04:42 +0200

    added save functions to AllCodebooksRepository

--- app/src/main/java/sk/msvvas/sofia/fam/offline/data/repository/codebook/AllCodebooksRepository.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/repository/codebook/AllCodebooksRepository.kt
@@ -1,15 +1,57 @@
 package sk.msvvas.sofia.fam.offline.data.repository.codebook
 
+import sk.msvvas.sofia.fam.offline.data.entities.codebook.*
+
 class AllCodebooksRepository(
-    localityCodebookRepository: LocalityCodebookRepository,
-    noteCodebookRepository: NoteCodebookRepository,
-    placesCodebookRepository: PlacesCodebookRepository,
-    roomCodebookRepository: RoomCodebookRepository,
-    userCodebookRepository: UserCodebookRepository
+    private val localityCodebookRepository: LocalityCodebookRepository,
+    private val noteCodebookRepository: NoteCodebookRepository,
+    private val placesCodebookRepository: PlacesCodebookRepository,
+    private val roomCodebookRepository: RoomCodebookRepository,
+    private val userCodebookRepository: UserCodebookRepository
 ) {
     val allLocalities = localityCodebookRepository.allData
     val allRooms = roomCodebookRepository.allData
     val allUsers = userCodebookRepository.allData
     val allPlaces = placesCodebookRepository.allData
     val allNotes = noteCodebookRepository.allData
+
+    fun saveLocality(locality: LocalityCodebookEntity) {
+        localityCodebookRepository.save(locality)
+    }
+
+    fun saveAllLocalities(localities: List<LocalityCodebookEntity>) {
+        localityCodebookRepository.saveAll(localities)
+    }
+
+    fun saveRoom(room: RoomCodebookEntity) {
+        roomCodebookRepository.save(room)
+    }
+
+    fun saveAllRooms(rooms: List<RoomCodebookEntity>) {
+        roomCodebookRepository.saveAll(rooms)
+    }
+
+    fun savePlace(place: PlacesCodebookEntity) {
+        placesCodebookRepository.save(place)
+    }
+
+    fun saveAllPlaces(places: List<PlacesCodebookEntity>) {
+        placesCodebookRepository.saveAll(places)
+    }
+
+    fun saveUser(user: UserCodebookEntity) {
+        userCodebookRepository.save(user)
+    }
+
+    fun saveAllUsers(users: List<UserCodebookEntity>) {
+        userCodebookRepository.saveAll(users)
+    }
+
+    fun saveNote(note: NoteCodebookEntity) {
+        noteCodebookRepository.save(note)
+    }
+
+    fun saveAllNotes(notes: List<NoteCodebookEntity>) {
+        noteCodebookRepository.saveAll(notes)
+    }
 }
