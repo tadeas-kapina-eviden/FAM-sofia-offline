ef4d4ce913b6b6c0f1429877c8e18a5f22db2deb
Kapina <tadeas.kapina@atos.net>
2022-09-01 19:46:31 +0200

    added Navigation for views managing

---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/navigation/Navigation.kt
@@ -0,0 +1,69 @@
+package sk.msvvas.sofia.fam.offline.ui.views.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import sk.msvvas.sofia.fam.offline.data.database.FamOfflineDatabase
+import sk.msvvas.sofia.fam.offline.data.repository.InventoryRepository
+import sk.msvvas.sofia.fam.offline.data.repository.PropertyRepository
+import sk.msvvas.sofia.fam.offline.data.repository.codebook.*
+import sk.msvvas.sofia.fam.offline.ui.views.inventory.detail.InventoryDetailView
+import sk.msvvas.sofia.fam.offline.ui.views.inventory.detail.InventoryDetailViewModel
+import sk.msvvas.sofia.fam.offline.ui.views.inventory.list.InventoryListView
+import sk.msvvas.sofia.fam.offline.ui.views.inventory.list.InventoryListViewModel
+import sk.msvvas.sofia.fam.offline.ui.views.login.LoginView
+import sk.msvvas.sofia.fam.offline.ui.views.login.LoginViewModel
+
+@Composable
+fun Navigation(
+    database: FamOfflineDatabase
+) {
+
+    val inventoryRepository = InventoryRepository(database.inventoryDao())
+    val propertyRepository = PropertyRepository(database.propertyDao())
+
+    val allCodebookRepository = AllCodebooksRepository(
+        localityCodebookRepository = LocalityCodebookRepository(database.localityCodebookDao()),
+        roomCodebookRepository = RoomCodebookRepository(database.roomCodebookDao()),
+        placesCodebookRepository = PlacesCodebookRepository(database.placesCodebookDao()),
+        userCodebookRepository = UserCodebookRepository(database.userCodebookDao()),
+        noteCodebookRepository = NoteCodebookRepository(database.noteCodebookDao())
+    )
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.LOGIN_VIEW.value) {
+        composable(route = Routes.LOGIN_VIEW.value) {
+            LoginView(loginViewModel = LoginViewModel(navController))
+        }
+        composable(route = Routes.INVENTORY_LIST.value) {
+            InventoryListView(
+                inventoryListViewModel = InventoryListViewModel(
+                    inventoryRepository = inventoryRepository,
+                    navController = navController
+                )
+            )
+        }
+        composable(
+            route = Routes.INVENTORY_DETAIL.value + "/{id}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = false
+                }
+            )
+        ) {
+            InventoryDetailView(
+                inventoryDetailViewModel = InventoryDetailViewModel(
+                    propertyRepository = propertyRepository,
+                    allCodebooksRepository = allCodebookRepository,
+                    navController = navController,
+                    inventoryIdParameter = it.arguments?.getString("name")!!
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/ui/views/navigation/Routes.kt
@@ -0,0 +1,17 @@
+package sk.msvvas.sofia.fam.offline.ui.views.navigation
+
+enum class Routes(val value: String) {
+    LOGIN_VIEW("login_view"),
+    INVENTORY_LIST("inventory_list"),
+    INVENTORY_DETAIL("inventory_detail"),
+    PROPERTY_DETAIL("property_detail");
+
+
+    fun withArgs(vararg args: String): String {
+        val route = StringBuilder(this.value)
+        args.forEach {
+            route.append("/$it")
+        }
+        return route.toString()
+    }
+}
