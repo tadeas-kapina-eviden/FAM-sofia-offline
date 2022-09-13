a92a7932ba2ff67a4e891a84c8a7cfb9a4bec0aa
Kapina <tadeas.kapina@atos.net>
2022-09-13 09:31:17 +0200

    reformat code - reuse functions

--- app/src/main/java/sk/msvvas/sofia/fam/offline/data/client/Client.kt
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/client/Client.kt
@@ -28,24 +28,12 @@ object Client {
     suspend fun validateLogin(username: String, password: String, clientId: String): Boolean {
         val client = HttpClient(CIO)
         val response: HttpResponse = client.get {
-            url {
-                protocol = PROTOCOL
-                host = HOST
-                path("sap", "opu", "odata", "vvs", "ZFAMFIORI_SRV", "getInventoryGeneralSet")
-                parameters.append("sap-client", clientId)
-                parameters.append("sap-language", "SK")
-                parameters.append(
-                    "Accept",
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                )
-                parameters.append("Accept-Encoding", "gzip, deflate, br")
-                parameters.append("Accept-Language", "sk")
-                parameters.append("Cache-Contro", "max-age=0")
-                parameters.append("Connection", "keep-alive")
-            }
-            basicAuth(
+            buildGetRequest(
+                this,
+                getPath = "getInventoryGeneralSet",
                 username = username,
-                password = password
+                password = password,
+                clientId = clientId
             )
         }
         client.close()
@@ -55,24 +43,9 @@ object Client {
     suspend fun getInventories(): List<InventoryEntity> {
         val client = HttpClient(CIO)
         val response: HttpResponse = client.get {
-            url {
-                protocol = PROTOCOL
-                host = HOST
-                path("sap", "opu", "odata", "vvs", "ZFAMFIORI_SRV", "getInventoryGeneralSet")
-                parameters.append("sap-client", ClientData.client)
-                parameters.append("sap-language", "SK")
-                parameters.append(
-                    "Accept",
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-                )
-                parameters.append("Accept-Encoding", "gzip, deflate, br")
-                parameters.append("Accept-Language", "sk")
-                parameters.append("Cache-Contro", "max-age=0")
-                parameters.append("Connection", "keep-alive")
-            }
-            basicAuth(
-                username = ClientData.username,
-                password = ClientData.password
+            buildGetRequest(
+                this,
+                getPath = "getInventoryGeneralSet"
             )
         }
         client.close()
@@ -91,4 +64,61 @@ object Client {
             }
         }
     }
+
+
+    private fun buildGetRequest(
+        builder: HttpRequestBuilder,
+        getPath: String,
+        username: String = ClientData.username,
+        password: String = ClientData.password,
+        clientId: String = ClientData.client
+    ) {
+        builder.let {
+            it.url {
+                buildUrl(this, getPath = getPath, clientId = clientId)
+            }
+            authorize(it, username = username, password = password)
+        }
+    }
+
+    private fun appendParameters(
+        parameters: ParametersBuilder,
+        clientId: String = ClientData.client
+    ) {
+        parameters.append("sap-client", clientId)
+        parameters.append("sap-language", "SK")
+        parameters.append(
+            "Accept",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        )
+        parameters.append("Accept-Encoding", "gzip, deflate, br")
+        parameters.append("Accept-Language", "sk")
+        parameters.append("Cache-Contro", "max-age=0")
+        parameters.append("Connection", "keep-alive")
+    }
+
+    private fun buildUrl(
+        builder: URLBuilder,
+        getPath: String,
+        clientId: String = ClientData.client
+    ) {
+        builder.let {
+            it.host = HOST
+            it.protocol = PROTOCOL
+            it.path(basePath, getPath)
+            appendParameters(it.parameters, clientId = clientId)
+        }
+    }
+
+    private fun authorize(
+        builder: HttpRequestBuilder,
+        username: String = ClientData.username,
+        password: String = ClientData.password
+    ) {
+        builder.basicAuth(
+            username = username,
+            password = password
+        )
+    }
+
 }
