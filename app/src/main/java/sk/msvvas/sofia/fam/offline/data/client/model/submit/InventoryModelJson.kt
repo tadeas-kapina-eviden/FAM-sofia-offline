830e3461455a0fa2ffd377628c04da3f72e5373e
Kapina <tadeas.kapina@atos.net>
2022-09-23 11:15:03 +0200

    added JSON models for properties for sending data to back-end

---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/client/model/submit/InventoryModelJson.kt
@@ -0,0 +1,32 @@
+package sk.msvvas.sofia.fam.offline.data.client.model.submit
+
+data class InventoryModelJson(
+    val id: String,
+    var createdAt: String,
+    var createdAtFormatted: String,
+    var createdBy: String,
+    var note: String,
+    var properties: List<PropertyModelJson>
+) {
+    fun toJSON(): String {
+        val propertiesEntriesStringBuilder = StringBuilder()
+        properties.forEach {
+            propertiesEntriesStringBuilder.append(it.toString())
+            propertiesEntriesStringBuilder.append(",\n")
+        }
+        propertiesEntriesStringBuilder.deleteCharAt(propertiesEntriesStringBuilder.lastIndex - 1)
+        return "{\n" +
+                "\t\"d\": {\n" +
+                "\t\t\"Inven\":\"$id\",\n" +
+                "\t\t\"Ipozn\":\"$note\",\n" +
+                "\t\t\"Datin\":\"$createdAt\",\n" +
+                "\t\t\"Datinf\":\"$createdAtFormatted\",\n" +
+                "\t\t\"Xubname\":\"$createdBy\",\n" +
+                "\t\t\"Text20\":\"todo\",\n" +
+                "\t\t\"GetInventoryItemsSet\":[\n" +
+                propertiesEntriesStringBuilder.toString() +
+                "\t\t]\n" +
+                "\t}\n" +
+                "}\n"
+    }
+}
---
+++ app/src/main/java/sk/msvvas/sofia/fam/offline/data/client/model/submit/PropertyModelJson.kt
@@ -0,0 +1,70 @@
+package sk.msvvas.sofia.fam.offline.data.client.model.submit
+
+data class PropertyModelJson(
+    var inventoryId: String,
+    var inventNumber: String,
+    var serialNumber: String,
+    var client: String,
+    var propertyNumber: String,
+    var subnumber: String,
+    var textMainNumber: String,
+    var recordStatus: Char,
+    var werks: String,
+    var locality: String,
+    var localityNew: String,
+    var room: String,
+    var roomNew: String,
+    var personalNumber: String,
+    var personalNumberNew: String,
+    var center: String,
+    var centerNew: String,
+    var workplace: String,
+    var workplaceNew: String,
+    var fixedNote: String,
+    var variableNote: String,
+    var isManual: Boolean,
+) {
+    fun toJSON(): String {
+        return "\t\t\t{\n" +
+                "\t\t\t\t\"Inven\":\"$inventoryId\",\n" +
+                "\t\t\t\t\"Invnr\":\"$inventNumber\",\n" +
+                "\t\t\t\t\"Sernr\":\"$serialNumber\",\n" +
+                "\t\t\t\t\"Zstattext\":\"${
+                    when (recordStatus) {
+                        'X' -> "Nespracovaný"
+                        'C' -> "Chýbajúci"
+                        'S' -> "Spracovaný"
+                        'Z' -> "Zmenený"
+                        'N' -> "Nový"
+                        else -> "Nespracovaný"
+                    }
+                }\",\n" +
+                "\t\t\t\t\"Mandt\":\"$client\",\n" +
+                "\t\t\t\t\"Bukrs\":\"1000\",\n" +
+                "\t\t\t\t\"Anln1\":\"$propertyNumber\",\n" +
+                "\t\t\t\t\"Anln2\":\"$subnumber\",\n" +
+                "\t\t\t\t\"Txt50\":\"$textMainNumber\",\n" +
+                "\t\t\t\t\"Zstat\":\"$recordStatus\",\n" +
+                "\t\t\t\t\"Werks\":\"$werks\",\n" +
+                "\t\t\t\t\"Werksn\":\"$werks\",\n" +
+                "\t\t\t\t\"Stort\":\"$locality\",\n" +
+                "\t\t\t\t\"Stortn\":\"$localityNew\",\n" +
+                "\t\t\t\t\"Raumn\":\"$room\",\n" +
+                "\t\t\t\t\"Raumnn\":\"$roomNew\",\n" +
+                "\t\t\t\t\"Pernr\":\"$personalNumber\",\n" +
+                "\t\t\t\t\"Pernrn\":\"$personalNumberNew\",\n" +
+                "\t\t\t\t\"Kostl\":\"$center\",\n" +
+                "\t\t\t\t\"Kostln\":\"$centerNew\",\n" +
+                "\t\t\t\t\"Anlue\":\"$workplace\",\n" +
+                "\t\t\t\t\"Anluen\":\"$workplaceNew\",\n" +
+                "\t\t\t\t\"Mengeo\":\"0.000\",\n" +
+                "\t\t\t\t\"Mengen\":\"0.000\",\n" +
+                "\t\t\t\t\"Xnote\":\"${if (fixedNote.isEmpty()) "" else fixedNote.first()}\",\n" +
+                "\t\t\t\t\"Fnote\":\"$variableNote\",\n" +
+                "\t\t\t\t\"Manua\":\"${
+                    if (isManual) "Y"
+                    else "N"
+                }\"\n" +
+                "\t\t\t}"
+    }
+}
